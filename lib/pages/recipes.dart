a52645485dd33d578c945b2ec6bc17391cc9b961
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-22 01:45:31 +0100

    feat: create container with meals

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../api/fetch_meals.dart';
+import '../model/meal_block.dart';
 
 class RecipesPage extends StatelessWidget {
   const RecipesPage({super.key});
@@ -6,11 +8,71 @@ class RecipesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text(
-          'Welcome to Recipes Page!',
-          style: TextStyle(fontSize: 24, color: Colors.black),
-        ),
+      appBar: AppBar(
+        title: Text('All Recipes'),
+        leading: SizedBox.shrink(),
+      ),
+      body: FutureBuilder<List<Meal>>(
+        future: fetchMealsByCategory('Pasta'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No recipes found'));
+          }
+
+          final meals = snapshot.data!;
+
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: meals.map((meal) {
+                return Container(
+                  width: 200,
+                  height: 250,
+                  margin: EdgeInsets.all(8),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipOval(
+                            child: Image.network(
+                              meal.imageUrl,
+                              fit: BoxFit.cover,
+                              height: 100,
+                              width: 100,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              meal.name,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          );
+        },
       ),
     );
   }
